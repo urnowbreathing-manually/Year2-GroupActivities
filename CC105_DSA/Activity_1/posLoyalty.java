12310ea70facdc5ddfb955c819f099dfa0555aff
tongaks <rrfrokuu@gmail.com>
2025-08-07 18:07:38 +0800

    added ask user for card id, display card id and points, add points to id

--- CC105_DSA/Activity_1/posLoyalty.java
+++ CC105_DSA/Activity_1/posLoyalty.java
@@ -1,44 +1,76 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Vector;
 
 class loyaltyAccounts{
-    String accountName;
     int accountNumber = 0;
     int loyaltyPoints = 0;
+    // Vector<String> productsSelected = new Vector<>();
 
-    public void account1(){
-
+    loyaltyAccounts(int cardID, int cur_p) {
+        this.accountNumber = cardID;
+        this.loyaltyPoints = cur_p;
     }
 
-    public void account2(){
-        
+    public int getPoints() {
+        return loyaltyPoints;
     }
 
-    public void account3(){
-        
+    public int getCardID() {
+        return accountNumber;
     }
 }
 
-public class posLoyalty extends loyaltyAccounts{
-
+public class posLoyalty {
     public static void main( String[] args){
         String[] productName = {"Choco Biscuits", "Organic Shampoo", "Instant Noodles", "Facial Cleanser", "Coffee Grounds", "Dishwashing Liquid", "Hand Sanitizer", "Tomato Sauce", "Dog Food (Beef)", "Floor Cleaner", "Laundry Detergent", "Sardines", "Air Freshener Spray", "Antibacterial Wipes", "Baby Powder", "Liquid Hand Soap", "Chocolate Bar", "Milk", "Cooking Oil", "Vitamin C Tablets"};
         int[] productAmount = {15, 1, 10, 5, 15, 1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1, 3, 1, 1, 1};
         double[] productPrices = {7.00, 150.25, 25.00, 90.50, 15.00, 189.75, 45.00, 25.00, 270.25, 283.50, 190.00, 35.25, 150.00, 48.75, 30.00, 63.50, 15.00, 120.50, 175.00, 90.75};
+        int[] productPoints = {1, 3, 1, 2, 3, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1};
         double totalPrice = 0.00;
-
-        posLoyalty loyaltyAccounts = new posLoyalty();
         
+        // generate random customer
+        Random rndg = new Random();
+        int cardID = rndg.nextInt(9999);
+        int currentPnts = rndg.nextInt(9999);
+        loyaltyAccounts acc = new loyaltyAccounts(cardID, currentPnts);
+        System.out.println("card id: " + acc.getCardID() + "\n points: " + acc.getPoints());
 
-        for(int i = 0; i < productName.length; i++){
-            totalPrice = totalPrice + productPrices[i];
+        // generate random items
+        int prod_cont = rndg.nextInt(productName.length);
+        Vector<String> prods_selected = new Vector<>();
+        Vector<Double> prods_price = new Vector<>();
+        Vector<Double> prods_total_price = new Vector<>();
+        Vector<Integer> prods_amount = new Vector<>();
+
+        for (int d = 0; d < prod_cont; d++) {
+            int num_gen = rndg.nextInt(productName.length);
+            int p_amount = rndg.nextInt(20);
+
+            prods_selected.addElement(productName[num_gen]);
+            prods_amount.addElement(p_amount);
+            prods_price.addElement(productPrices[num_gen]);
+            prods_total_price.addElement(productPrices[num_gen] * p_amount);
         }
 
+        for (double price : prods_total_price) {
+            totalPrice += price;
+        }
+
+        // System.out.println("\nProds selected list\n");
+        // for(String p : prods_selected) {
+        //     System.out.println(p);
+        // }
+
         System.out.println("[]==================[ POS Checkout Receipt ]==================[]\n  Item/s for Checkout:");
-        for(int i = 0; i < productName.length; i++){
+
+        int i = 0;
+        for (String product : prods_selected) {
             String tabCorrection[] = {"", ""};
-            if(productName[i].length() < 8){
+            if(product.length() < 8){
                 tabCorrection[0] = "\t\t\t";
                 tabCorrection[1] = "\t";
-            } else if(productName[i].length() >= 8 && productName[i].length() < 16){
+            } else if(product.length() >= 8 && product.length() < 16){
                 tabCorrection[0] = "\t\t";
                 tabCorrection[1] = "\t";
             } else{
@@ -54,12 +86,33 @@ public class posLoyalty extends loyaltyAccounts{
             }
 
 
-            System.out.println("    " + (i + 1) + ".) " + productName[i] + tabCorrection[0] +"|| x" + productAmount[i] + " " + unitPluralCheck + tabCorrection[1] +"|| Php " + String.format("%.2f", productPrices[i]));
+            System.out.println("    " + (i + 1) + ".) " + product + tabCorrection[0] +"|| x" + prods_amount.get(i) + " " + unitPluralCheck + tabCorrection[1] + "|| Php " + String.format("%.2f", prods_price.get(i)) + " || Php " + String.format("%.2f", prods_total_price.get(i)));
+            i+=1;
         }
 
         System.out.println("\n[]=====================[ Subtotal Price ]=====================[]");
-        System.out.println("  Subtotal Price:\n    [ Php" + String.format("%.2f", totalPrice) + " ]\n");
-        System.out.println("[]=====================[ Loyalty  Rewards ]=====================[]\n  Loyalty Rewards System:\n    + Does the user have a Loyalty Rewards Card? [Yy/Nn]: ");
+        System.out.println("  Subtotal Price:\n    [ Php " + String.format("%.2f", totalPrice) + " ]\n");
+
+        Scanner card_input = new Scanner(System.in);
+        System.out.print("Enter the card ID here (Type N|n if no card): ");
+        int res = card_input.nextInt();
+
+        if (res == acc.getCardID()) {
+            System.out.println("\n[]=====================[ Loyalty  Rewards ]=====================[]");
+
+            int totalPoints = 0;
+            for (int j = 0; j < productName.length; j++) {
+                totalPoints += rndg.nextInt(5) + 1;
+            }
+
+            System.out.println("\tCurrent Points: " + acc.getPoints());
+            System.out.println("\tPoints to receive: " + totalPoints);
+            System.out.println("\tTotal Points: " + (totalPoints + acc.getPoints()));
+
+            System.out.println("[]=====================[ Loyalty  Rewards ]=====================[]\n");
+        } else {
+            System.out.println("No card: " + res);
+        }
 
     }
 }
